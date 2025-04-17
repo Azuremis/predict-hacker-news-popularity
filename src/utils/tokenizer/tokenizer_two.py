57ef44478b97d25ce50d5a752473631999456e2e
Jason Warren <contact.jwarren@gmail.com>
2025-04-17 16:54:58 +0100

    create hacker news tokens

--- src/utils/tokenizer/tokenizer_two.py
+++ src/utils/tokenizer/tokenizer_two.py
@@ -39,31 +39,60 @@ conn = psycopg2.connect(
     password=os.getenv("PGPASSWORD")
 )
 cursor = conn.cursor()
-cursor.execute("SELECT title FROM hacker_news.items_by_month_2024_10 WHERE type = 'story' AND title IS NOT NULL")
+cursor.execute("SELECT title FROM hacker_news.items WHERE type = 'story' AND title IS NOT NULL")
 
 chunk_size = 1000
+total_listed = 0
 total_processed = 0
 
 while True:
-    rows = cursor.fetchmany(chunk_size)
+    rows = cursor.fetchall()
     if not rows:
         break
 
     logging.info("Tokenizing titles...")
+
+    morpheme_list = []
+    last_logged = 0
+
     for title, in rows:
         if not title:
             continue
         morphemes = getMorphemeList(title)
-        morpheme_set = getMorphemeSet(morphemes)
-        tokens = getTokens(morpheme_set)
-        for token in tokens:
-            if token not in token_dict:
-                token_dict[token] = str(next_id)
-                next_id += 1
-        total_processed += 1
-        if total_processed % 1000 == 0:
-            logging.info(f"Processed {total_processed} titles so far")
-            logging.info(f"Holding {len(token_dict)} tokens so far")
+        morpheme_list.extend(morphemes)
+        if len(morpheme_list) % 1000 == 0 and len(morpheme_list) > last_logged:
+            logging.info(f"Holding {len(morpheme_list)} words in list")
+            last_logged = len(morpheme_list)
+    
+    morpheme_set = getMorphemeSet(morpheme_list)
+
+    if len(morpheme_set) > 0:
+        logging.info(f"Holding {len(morpheme_set)} words in set")
+
+    number_added = 0
+
+    for morpheme in morpheme_set:
+        if morpheme not in token_dict:
+            token_dict[morpheme] = int(next_id)
+            number_added += 1
+            next_id += 1
+    
+    logging.info(f"Added {number_added} tokens")
+    
+    # for title, in rows:
+    #     if not title:
+    #         continue
+    #     morphemes = getMorphemeList(title)
+    #     morpheme_set = getMorphemeSet(morphemes)
+    #     tokens = getTokens(morpheme_set)
+    #     for token in tokens:
+    #         if token not in token_dict:
+    #             token_dict[token] = str(next_id)
+    #             next_id += 1
+    #     total_processed += 1
+    #     if total_processed % 1000 == 0:
+    #         logging.info(f"Processed {total_processed} titles so far")
+    #         logging.info(f"Holding {len(token_dict)} tokens so far")
 
 with open(os.path.join(
     os.path.dirname(
